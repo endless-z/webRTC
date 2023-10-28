e4029b392a3563547a244397586454def81a81c7
柠条 <ningtiao@jushuitan.com>
2023-10-28 23:04:03 +0800

    feat: 页面布局

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# README
+# WebRTC
 
 `@umijs/max` 模板项目，更多功能参考 [Umi Max 简介](https://umijs.org/docs/max/introduce)
+
+### 1、什么是 WebRTC
+
+WebRTC（Web Real-Time Communications）是一项实时通讯技术，它允许网络应用或者站点，在不借助中间媒介的情况下，建立浏览器之间点对点（Peer-to-Peer）的连接，实现视频流和（或）音频流或者其他任意数据的传输。WebRTC 包含的这些标准使用户在无需安装任何插件或者第三方的软件的情况下，创建点对点（Peer-to-Peer）的数据分享和电话会议成为可能。
+
+### 2、WebRTC 的应用场景
+
+WebRTC 的能力使其适用于各种实时通信场景：
+
+- 点对点通讯：WebRTC 支持浏览器之间进行音视频通话，例如语音通话、视频通话等；
+- 电话会议：WebRTC 可以支持多人音视频会议，例如腾讯会议、钉钉会议等；
+- 屏幕共享：WebRTC 不仅可以传输音视频流，还可以用于实时共享屏幕；
+- 直播：WebRTC 可以用于构建实时直播，用户可以通过浏览器观看直播内容。
--- src/components/Guide/Guide.tsx
+++ src/components/Guide/Guide.tsx
@@ -1,7 +1,5 @@
-import { Layout, Row, Typography } from 'antd';
+import { Button, Col, Form, Input, Layout, Row, Select } from 'antd';
 import React from 'react';
-import styles from './Guide.less';
-
 interface Props {
   name: string;
 }
@@ -9,14 +7,64 @@ interface Props {
 // 脚手架示例组件
 const Guide: React.FC<Props> = (props) => {
   const { name } = props;
+  const [form] = Form.useForm();
+
   return (
-    <Layout>
-      <Row>
-        <Typography.Title level={3} className={styles.title}>
-          欢迎使用 <strong>{name}</strong> ！
-        </Typography.Title>
-      </Row>
-    </Layout>
+    <div
+      style={{ background: '#f7f8fa', padding: '10px', borderRadius: '4px' }}
+    >
+      <Layout>
+        <Form form={form} layout="inline">
+          <Row gutter={[16, 8]}>
+            <Col>
+              <Form.Item label="选择摄像头" name="layout">
+                <Select
+                  defaultValue="lucy"
+                  style={{ width: 120 }}
+                  options={[
+                    { value: 'jack', label: 'Jack' },
+                    { value: 'lucy', label: 'Lucy' },
+                    { value: 'Yiminghe', label: 'yiminghe' },
+                    { value: 'disabled', label: 'Disabled', disabled: true },
+                  ]}
+                />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item label="选择麦克风">
+                <Input placeholder="选择麦克风" />
+              </Form.Item>
+            </Col>
+
+            <Col>
+              <Form.Item label="选择听筒">
+                <Input placeholder="选择听筒" />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item label="分辨率Width">
+                <Input placeholder="分辨率Width" />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item label="分辨率Height">
+                <Input placeholder="分辨率Height" />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item label="PFS">
+                <Input placeholder="PFS" />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item>
+                <Button type="primary">确定</Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Layout>
+    </div>
   );
 };
 
