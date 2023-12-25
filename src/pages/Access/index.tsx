7b922d1a3eb7c528c694eaa63731c83a25614ab2
yangjiaxin <578755236@qq.com>
2023-12-25 10:47:52 +0800

    feat: 视频通话

--- src/pages/Access/index.tsx
+++ src/pages/Access/index.tsx
@@ -1,10 +1,16 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { Access, useAccess } from '@umijs/max';
 import { Button } from 'antd';
-import Test from './components/test';
+import { useState } from 'react';
+import MessageBtn from './components/MessageBtn';
 
 const AccessPage: React.FC = () => {
+  const [show, setShow] = useState(true);
   const access = useAccess();
+
+  const handleClick = () => {
+    setShow(!show);
+  };
 
   return (
     <PageContainer
@@ -16,7 +22,12 @@ const AccessPage: React.FC = () => {
       <Access accessible={access.canSeeAdmin}>
         <Button>只有 Admin 可以看到这个按钮</Button>
       </Access>
-      <Test />
+      <div style={{ marginTop: '20px' }}>
+        {show && <MessageBtn />}
+
+        <Button onClick={handleClick}>卸载</Button>
+      </div>
+      {/* <Test /> */}
     </PageContainer>
   );
 };
