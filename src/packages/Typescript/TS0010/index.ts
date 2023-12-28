9fbe8c2aa04e532f4f17b886060a47637c172ccb
yangjiaxin <578755236@qq.com>
2023-12-28 11:46:13 +0800

    feat: TS 泛型

---
+++ src/packages/Typescript/TS0010/index.ts
@@ -0,0 +1,57 @@
+/**
+ * TS 泛型
+ */
+
+type IFactory<T> = T | number | string;
+
+type IStringify<T> = {
+  [K in keyof T]: string;
+};
+
+type IClone<T> = {
+  [K in keyof T]: T[K];
+};
+
+// Stringify 会将一个对象类型的所有属性类型置为 string ，而 Clone 则会进行类型的完全复制。
+// TypeScript 的内置工具类型实现：
+type IPartial<T> = {
+  [P in keyof T]?: T[P];
+};
+
+interface IFoo {
+  prop1: string;
+  prop2: number;
+  prop3: boolean;
+  prop4: () => void;
+}
+
+type PartialIFoo = IPartial<IFoo>;
+
+// 等价于
+interface IPartialIFoo {
+  prop1?: string;
+  prop2?: number;
+  prop3?: boolean;
+  prop4?: () => void;
+}
+
+// 条件类型
+
+type IsEqual<T> = T extends true ? 1 : 2;
+
+type IA = IsEqual<true>;
+
+type IB = IsEqual<false>;
+
+type IC = IsEqual<'ningtiao'>;
+
+type FactoryT<T = boolean> = T | number | string;
+
+/**
+ * 开发 master----->feature------>develop
+ * 发布 feature------> release ----> master
+ * bugfix master----->hotfix---->master
+ *
+ *
+ *
+ */
